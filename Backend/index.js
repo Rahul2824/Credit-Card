0209ab7523dbb05ee94a910bac5ce1b0ee2ca11e
Rahul patil <rahulpatil121734@gmail.com>
2026-09-14 23:39:11 +0530

    fix railway backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,8 @@
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+dns.setDefaultResultOrder("ipv4first");
+
 const express = require("express");
 const { MongoClient } = require("mongodb");
 const cors = require("cors");
